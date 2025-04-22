aa72e7e123832373c739be576f2984dbd70f4f6e
LightX101 <dipaktigga1011@gmail.com>
2025-04-22 22:40:47 +0530

    mm

--- src/components/ChooseUs.jsx
+++ src/components/ChooseUs.jsx
@@ -16,15 +16,10 @@ function ChooseUs() {
             />
             <div className="text-container">
               <div className="text-container__left">
-                <h4>Why Choose Us</h4>
-                <h2>Best valued deals you will ever find</h2>
+                <h4>Why Choose Us?</h4>
+                <h2>Unbeatable Value & Deals</h2>
                 <p>
-                  Discover the best deals you'll ever find with our unbeatable
-                  offers. We're dedicated to providing you with the best value
-                  for your money, so you can enjoy top-quality services and
-                  products without breaking the bank. Our deals are designed to
-                  give you the ultimate renting experience, so don't miss out on
-                  your chance to save big.
+                Discover exceptional deals that offer the best value for your money. We're committed to bringing you the most competitive prices, ensuring you get top-quality services and products without exceeding your budget. Our exclusive offers are designed to give you the best renting experience possible, so don't miss out on the chance to save big!
                 </p>
                 <a href="#home">
                   Find Details &nbsp;
@@ -35,10 +30,9 @@ function ChooseUs() {
                 <div className="text-container__right__box">
                   <img src={Box1} alt="car-img" />
                   <div className="text-container__right__box__text">
-                    <h4>Cross Country Drive</h4>
+                    <h4>Cross Country</h4>
                     <p>
-                      Take your driving experience to the next level with our
-                      top-notch vehicles for your cross-country adventures.
+                    Upgrade your journey with our premium vehicles, designed for unforgettable cross-country adventures. Experience smooth rides and scenic routes like never before.
                     </p>
                   </div>
                 </div>
@@ -46,10 +40,10 @@ function ChooseUs() {
                   {" "}
                   <img src={Box2} alt="coin-img" />
                   <div className="text-container__right__box__text">
-                    <h4>All Inclusive Pricing</h4>
+                    <h4>Transparent Pricing</h4>
                     <p>
-                      Get everything you need in one convenient, transparent
-                      price with our all-inclusive pricing policy.
+                    Get everything you need at one clear price with no hidden fees. Our all-inclusive pricing ensures a hassle-free experience from start to finish.
+
                     </p>
                   </div>
                 </div>
@@ -57,10 +51,9 @@ function ChooseUs() {
                   {" "}
                   <img src={Box3} alt="coin-img" />
                   <div className="text-container__right__box__text">
-                    <h4>No Hidden Charges</h4>
+                    <h4>Protection You Can Trust</h4>
                     <p>
-                      Enjoy peace of mind with our no hidden charges policy. We
-                      believe in transparent and honest pricing.
+                    Drive with confidence knowing you're covered by our comprehensive protection plan. We prioritize your safety and peace of mind with transparent and reliable coverage.
                     </p>
                   </div>
                 </div>
