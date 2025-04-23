48b374c6bbaaaa3e1f2d5be0c6eb8545e8dae11c
LightX101 <dipaktigga1011@gmail.com>
2025-04-23 12:46:37 +0530

    about section done

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,7 +1,7 @@
 import Footer from "../components/Footer";
 import HeroPages from "../components/HeroPages";
 import PlanTrip from "../components/PlanTrip";
-import AboutMain from "../images/about/about-main.jpg";
+import AboutMain from "../images/about/about-main.png";
 import Box1 from "../images/about/icon1.png";
 import Box2 from "../images/about/icon2.png";
 import Box3 from "../images/about/icon3.png";
@@ -10,7 +10,7 @@ function About() {
   return (
     <>
       <section className="about-page">
-        <HeroPages name="About" />
+        <HeroPages name="About" />  
         <div className="container">
           <div className="about-main">
             <img
@@ -19,34 +19,35 @@ function About() {
               alt="car-renting"
             />
             <div className="about-main__text">
-              <h3>About Company</h3>
-              <h2>You start the engine and your adventure begins</h2>
+              <h3>About DriveAway</h3>
+              <h2>Start Your Journey with Confidence</h2>
               <p>
-                Certain but she but shyness why cottage. Guy the put instrument
-                sir entreaties affronting. Pretended exquisite see cordially the
-                you. Weeks quiet do vexed or whose. Motionless if no to
-                affronting imprudence no precaution. My indulged as disposal
-                strongly attended.
+                At DriveAway, we believe every journey should begin with ease
+                and excitement. As a trusted name in car rentals, we offer a
+                wide range of well-maintained vehicles tailored for every type
+                of traveler—from solo explorers to family road trippers. <br />
+                DriveAway isn’t just about getting from one place to another—it’s about making every drive count.
               </p>
+              
               <div className="about-main__text__icons">
                 <div className="about-main__text__icons__box">
                   <img src={Box1} alt="car-icon" />
                   <span>
-                    <h4>20</h4>
+                    <h4>30+</h4>
                     <p>Car Types</p>
                   </span>
                 </div>
                 <div className="about-main__text__icons__box">
                   <img src={Box2} alt="car-icon" />
                   <span>
-                    <h4>85</h4>
+                    <h4>20</h4>
                     <p>Rental Outlets</p>
                   </span>
                 </div>
                 <div className="about-main__text__icons__box">
                   <img src={Box3} alt="car-icon" className="last-fk" />
                   <span>
-                    <h4>75</h4>
+                    <h4>100</h4>
                     <p>Repair Shop</p>
                   </span>
                 </div>
@@ -60,10 +61,10 @@ function About() {
         <div className="book-banner__overlay"></div>
         <div className="container">
           <div className="text-content">
-            <h2>Book a car by getting in touch with us</h2>
+            <h2>Contact us now to reserve your vehicle</h2>
             <span>
               <i className="fa-solid fa-phone"></i>
-              <h3>(123) 456-7869</h3>
+              <h3>(91)123-456-7890</h3>
             </span>
           </div>
         </div>
